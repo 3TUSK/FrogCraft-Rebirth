275e87d6c7c85c1886c7f2cd598b74401a112bc5
3TUSK <urey.s.knowledge@gmail.com>
2016-04-03 16:04:45 -0400

    Use gradle to handle version number

--- src/main/java/frogcraftrebirth/common/lib/FrogRef.java
+++ src/main/java/frogcraftrebirth/common/lib/FrogRef.java
@@ -6,8 +6,6 @@ public class FrogRef {
 	
 	public static final String NAME = "FrogCraft: Rebirth";
 	
-	public static final String VERSION = "0.4.0.0 Beta";
-	
 	public static final String DEPENDING = 
 			"required-after:IC2;"
 			+ "after:gregtech;"

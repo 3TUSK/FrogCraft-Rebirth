c971eaffc3ae8b9289ecdd2f575235516b37b7d1
3TUSK <urey.s.knowledge@gmail.com>
2016-04-13 21:10:52 -0400

    Fix achievements again.
    Signed-off-by: 3TUSK <urey.s.knowledge@gmail.com>

--- src/main/java/frogcraftrebirth/common/registry/RegFrogAchievements.java
+++ src/main/java/frogcraftrebirth/common/registry/RegFrogAchievements.java
@@ -1,8 +1,6 @@
 package frogcraftrebirth.common.registry;
 
-import java.util.LinkedHashMap;
-
-import cpw.mods.fml.common.Loader;
+//import cpw.mods.fml.common.Loader;
 import frogcraftrebirth.api.FrogAPI;
 import frogcraftrebirth.common.FrogBlocks;
 import frogcraftrebirth.common.FrogItems;
@@ -12,42 +10,23 @@ import net.minecraftforge.common.AchievementPage;
 
 public class RegFrogAchievements {
 	
-	static LinkedHashMap<String, Achievement> map = new LinkedHashMap<>();
-	static AchievementPage frogPage;
-	
 	public static void init() {
-		map.put("EVT", EVT);
-		map.put("RAILGUN", RAILGUN);
-		map.put("POTASSIUM", POTASSIUM);
-		map.put("GAS_PUMP", GAS_PUMP);
-		map.put("LIQUIFIER ", LIQUIFIER);
-		map.put("HSU", HSU);
-		map.put("UHSU", UHSU);
-		map.put("ADV_CHEM_REACTOR", ADV_CHEM_REACTOR);
-		map.put("JINKELA", JINKELA);
-		map.put("CONDENSE_TOWER_CORE", CONDENSE_TOWER_CORE);
-		map.put("CONDENSE_TOWER", CONDENSE_TOWER);
-		map.put("NITRIC_ACID", NITRIC_ACID);
-		
-		if (Loader.isModLoaded("gregtech"))
-			map.put("PNEUMATIC_COMPRESSOR", PNEUMATIC_COMPRESSOR);
-		
-		frogPage = new AchievementPage(FrogAPI.MODID, (Achievement[]) map.values().toArray());
-		AchievementPage.registerAchievementPage(frogPage);
+		AchievementPage.registerAchievementPage(new AchievementPage(FrogAPI.MODID, 
+				EVT, RAILGUN, POTASSIUM, GAS_PUMP, PNEUMATIC_COMPRESSOR, LIQUIFIER, HSU, UHSU, ADV_CHEM_REACTOR, JINKELA, CONDENSE_TOWER_CORE, CONDENSE_TOWER, NITRIC_ACID));
 	}
 	
-	static final Achievement EVT = new Achievement("EVT", "EVT", 0, 0, (ItemStack)null, null);
-	static final Achievement RAILGUN = new Achievement("RAILGUN", "railgun", 0, -2, new ItemStack(FrogItems.railgun), null);
-	static final Achievement POTASSIUM = new Achievement("POTASSIUM", "potassiumExplosion", -2, 0, new ItemStack(FrogItems.itemIngot, 1, 0), null);
-	static final Achievement GAS_PUMP = new Achievement("GAS_PUMP", "gasPump", 2, 0, new ItemStack(FrogBlocks.machines, 1, 1), null);
+	static final Achievement EVT = new Achievement("EVT", "get_EVT", 0, 0, new ItemStack(FrogBlocks.hybridStorageUnit), null);
+	static final Achievement RAILGUN = new Achievement("RAILGUN", "get_railgun", 0, -2, new ItemStack(FrogItems.railgun), null);
+	static final Achievement POTASSIUM = new Achievement("POTASSIUM", "killed_by_Potassium", -2, 0, new ItemStack(FrogItems.itemIngot, 1, 0), null);
+	static final Achievement GAS_PUMP = new Achievement("GAS_PUMP", "gaspump", 2, 0, new ItemStack(FrogBlocks.machines, 1, 1), null);
 	static final Achievement PNEUMATIC_COMPRESSOR = new Achievement("PNEUMATIC_COMPRESSOR", "pneumaticCompressor", 2, 2, FrogItems.pneumaticCompressor, GAS_PUMP);
 	static final Achievement LIQUIFIER = new Achievement("LIQUIFER", "liquifier", 2, -2, new ItemStack(FrogBlocks.machines), GAS_PUMP);
-	static final Achievement HSU = new Achievement("HSU", "HSU", 0, 2, new ItemStack(FrogBlocks.hybridStorageUnit), EVT);
-	static final Achievement UHSU = new Achievement("UHSU", "UHSU", 0, 4, new ItemStack(FrogBlocks.hybridStorageUnit, 1, 1), HSU);
-	static final Achievement ADV_CHEM_REACTOR = new Achievement("ADV_CHEM_REACTOR", "advChemReactor", 4, 0, new ItemStack(FrogBlocks.machines, 1, 0), GAS_PUMP);
-	static final Achievement JINKELA = new Achievement("JINKELA", "jinkela", 4, -2, new ItemStack(FrogItems.jinkela), ADV_CHEM_REACTOR);
-	static final Achievement CONDENSE_TOWER_CORE = new Achievement("CONDENSE_TOWER_CORE", "condenseTowerCore", 2, -4, new ItemStack(FrogBlocks.condenseTowerPart), LIQUIFIER);
-	static final Achievement CONDENSE_TOWER = new Achievement("CONDENSE_TOWER", "condenseTowerWall", 4, -4, new ItemStack(FrogBlocks.condenseTowerPart), CONDENSE_TOWER_CORE);
-	static final Achievement NITRIC_ACID = new Achievement("NITRIC_ACID", "nitricAcidExplodsion", -2, 2, new ItemStack(FrogBlocks.fluidNitricAcid), POTASSIUM);
+	static final Achievement HSU = new Achievement("HSU", "hsu", 0, 2, new ItemStack(FrogBlocks.hybridStorageUnit), EVT);
+	static final Achievement UHSU = new Achievement("UHSU", "uhsu", 0, 4, new ItemStack(FrogBlocks.hybridStorageUnit, 1, 1), HSU);
+	static final Achievement ADV_CHEM_REACTOR = new Achievement("ADV_CHEM_REACTOR", "get_ACR", 4, 0, new ItemStack(FrogBlocks.machines, 1, 0), GAS_PUMP);
+	static final Achievement JINKELA = new Achievement("JINKELA", "goldclod", 4, -2, new ItemStack(FrogItems.jinkela), ADV_CHEM_REACTOR);
+	static final Achievement CONDENSE_TOWER_CORE = new Achievement("CONDENSE_TOWER_CORE", "condensetower", 2, -4, new ItemStack(FrogBlocks.condenseTowerPart), LIQUIFIER);
+	static final Achievement CONDENSE_TOWER = new Achievement("CONDENSE_TOWER", "condensetower2", 4, -4, new ItemStack(FrogBlocks.condenseTowerPart), CONDENSE_TOWER_CORE);
+	static final Achievement NITRIC_ACID = new Achievement("NITRIC_ACID", "suicideExpert", -2, 2, new ItemStack(FrogBlocks.fluidNitricAcid), POTASSIUM);
 
 }

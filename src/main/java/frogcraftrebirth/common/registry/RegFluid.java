124d2a71e5613402d0f4a1211f620f3b5b9c3127
3TUSK <urey.s.knowledge@gmail.com>
2016-07-22 22:21:45 +0800

    Still not working

--- src/main/java/frogcraftrebirth/common/registry/RegFluid.java
+++ src/main/java/frogcraftrebirth/common/registry/RegFluid.java
@@ -3,6 +3,7 @@ package frogcraftrebirth.common.registry;
 import frogcraftrebirth.common.FrogBlocks;
 import frogcraftrebirth.common.FrogFluids;
 import frogcraftrebirth.common.block.BlockNitricAcid;
+//import frogcraftrebirth.common.lib.item.ItemFrogBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.util.ResourceLocation;
@@ -12,32 +13,28 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class RegFluid {
 	
-	private static ResourceLocation getTexture(String name, boolean flow) {
-		return new ResourceLocation("frogcraftrebirth:textures/blocks/fluids/"+name+".png");
-	}
-	
 	public static void init() {
-		FrogFluids.ammonia = new Fluid("ammonia", getTexture("Ammonia", false), getTexture("Ammonia", true))
+		FrogFluids.ammonia = new Fluid("ammonia", getTexture("ammonia", false), getTexture("ammonia", true))
 				.setDensity(694).setRarity(EnumRarity.EPIC).setGaseous(true);
-		FrogFluids.argon = new Fluid("argon", getTexture("Argon", false), getTexture("Argon", true))
+		FrogFluids.argon = new Fluid("argon", getTexture("argon", false), getTexture("argon", true))
 				.setDensity(1784).setRarity(EnumRarity.RARE).setGaseous(true);
-		FrogFluids.benzene = new Fluid("benzene", getTexture("Benzene", false), getTexture("Benzene", true))
+		FrogFluids.benzene = new Fluid("benzene", getTexture("benzene", false), getTexture("benzene", true))
 				.setDensity(877).setRarity(EnumRarity.EPIC).setGaseous(true);
-		FrogFluids.bromine = new Fluid("bromine", getTexture("Bromine", false), getTexture("Bromine", true))
+		FrogFluids.bromine = new Fluid("bromine", getTexture("bromine", false), getTexture("bromine", true))
 				.setDensity(3103).setRarity(EnumRarity.UNCOMMON);
 		FrogFluids.carbonOxide = new Fluid("carbonOxide", getTexture("CO", false), getTexture("CO", true))
 				.setDensity(1250).setRarity(EnumRarity.UNCOMMON).setGaseous(true);
 		FrogFluids.carbonDioxide = new Fluid("carbonDioxide", getTexture("CO2", false), getTexture("CO2", true))
 				.setDensity(1980).setRarity(EnumRarity.COMMON).setGaseous(true);
-		FrogFluids.coalTar = new Fluid("coalTar", getTexture("CoalTar", false), getTexture("CoalTar", true))
+		FrogFluids.coalTar = new Fluid("coalTar", getTexture("coalTar", false), getTexture("coalTar", true))
 				.setRarity(EnumRarity.RARE).setViscosity(2000);
-		FrogFluids.fluorine = new Fluid("flourine", getTexture("Fluorine", false), getTexture("Fluorine", true))
+		FrogFluids.fluorine = new Fluid("fluorine", getTexture("fluorine", false), getTexture("fluorine", true))
 				.setDensity(1696).setRarity(EnumRarity.EPIC).setGaseous(true);
 		FrogFluids.nitricAcid = new Fluid("nitricAcid", getTexture("HNO3", false), getTexture("HNO3", true))
 				.setDensity(1420).setRarity(EnumRarity.RARE);
 		FrogFluids.nitrogenOxide = new Fluid("nitrogenOxide", getTexture("NO", false), getTexture("NO", true))
 				.setDensity(1340).setRarity(EnumRarity.RARE).setGaseous(true);
-		FrogFluids.oxygen = new Fluid("oxygen", getTexture("Oxygen", false), getTexture("Oxygen", true))
+		FrogFluids.oxygen = new Fluid("oxygen", getTexture("oxygen", false), getTexture("oxygen", true))
 				.setDensity(1429).setRarity(EnumRarity.COMMON).setGaseous(true);
 		FrogFluids.sulfurDioxide = new Fluid("sulfurDioxide", getTexture("SO2", false), getTexture("SO2", true))
 				.setRarity(EnumRarity.UNCOMMON).setGaseous(true);
@@ -58,9 +55,14 @@ public class RegFluid {
 		regFluid(FrogFluids.sulfurDioxide);
 		regFluid(FrogFluids.sulfurTrioxide);
 		
-		FrogBlocks.fluidNitricAcid = new BlockNitricAcid(FrogFluids.nitricAcid);
-		GameRegistry.<Block>register(FrogBlocks.fluidNitricAcid.setRegistryName("nitric_acid"));
+		FrogBlocks.fluidNitricAcid = new BlockNitricAcid(FrogFluids.nitricAcid).setRegistryName("nitric_acid");
+		GameRegistry.<Block>register(FrogBlocks.fluidNitricAcid);
 		FrogFluids.nitricAcid.setBlock(FrogBlocks.fluidNitricAcid);
+		//ItemFrogBlock.initItemBlock();
+	}
+	
+	private static ResourceLocation getTexture(String name, boolean flow) {
+		return new ResourceLocation("frogcraftrebirth", "blocks/fluids/" + name);
 	}
 	
 	private static void regFluid(Fluid fluid) {

f555dcf0bae00e99a3afeb25564de58be8da0e3f
3TUSK <urey.s.knowledge@gmail.com>
2016-07-23 22:19:24 +0800

    Event listener now should work somehow

--- src/main/java/frogcraftrebirth/common/FrogEventListener.java
+++ src/main/java/frogcraftrebirth/common/FrogEventListener.java
@@ -1,19 +1,19 @@
 package frogcraftrebirth.common;
 
+import frogcraftrebirth.api.FrogAchievements;
 import frogcraftrebirth.common.item.ItemIngot;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.event.entity.player.FillBucketEvent;
 import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemCraftedEvent;
 
 public class FrogEventListener {
 	
-	//TODO: achievement system.
 	@SubscribeEvent
 	public void onExplosion(ExplosionEvent event) {
 		if (event.getWorld().isRemote) 
@@ -22,17 +22,20 @@ public class FrogEventListener {
 		IBlockState block = event.getWorld().getBlockState(new BlockPos(event.getExplosion().getPosition()));
 		
 		if (item instanceof EntityItem){
-			if (((EntityItem)item).getEntityItem().getItem() instanceof ItemIngot&& ((EntityItem)item).getEntityItem().getItemDamage() == 0){
-				if (block == FrogBlocks.fluidNitricAcid);
-					item.entityDropItem(new ItemStack(Items.BLAZE_ROD), 0.5F);
-					//TODO
+			if (((EntityItem)item).getEntityItem().getItem() instanceof ItemIngot && ((EntityItem)item).getEntityItem().getItemDamage() == 0){
+				if (block == FrogBlocks.fluidNitricAcid) {
+					EntityPlayer player = event.getWorld().getClosestPlayerToEntity(item, 10.0D);
+					player.addStat(FrogAchievements.POTASSIUM.get());
+				}
 			}			
 		}
 	}
 	
 	@SubscribeEvent
-	public void onFillingBucket(FillBucketEvent event) {
-		//TODO
+	public void onCrafting(ItemCraftedEvent event) {
+		if (event.crafting.getItem() instanceof ItemBlock) {
+			//TODO
+		}
 	}
 
 }

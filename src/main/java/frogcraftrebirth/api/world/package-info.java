f5437bf6ac5621c3e97b78f3bb9fae3c5fd11fac
3TUSK <urey.s.knowledge@gmail.com>
2016-07-25 11:54:40 +0800

    Clean up API, so that there won't be spam in fml log
    Also remove IInductionalMachine. That will be a new mod.

--- src/main/java/frogcraftrebirth/api/world/package-info.java
+++
@@ -1,5 +0,0 @@
-@API(apiVersion = FrogAPI.API_VER, owner = FrogAPI.MODID, provides = FrogAPI.API)
-package frogcraftrebirth.api.world;
-
-import frogcraftrebirth.api.FrogAPI;
-import net.minecraftforge.fml.common.API;

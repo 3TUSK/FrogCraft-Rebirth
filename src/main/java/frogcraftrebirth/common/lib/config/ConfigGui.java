27c69cb9e6d6fd09ac00a8c923827159e4cb0122
3TUSK <urey.s.knowledge@gmail.com>
2016-07-25 20:19:47 +0800

    Damnit gitignore, here is the ConfigGui

---
+++ src/main/java/frogcraftrebirth/common/lib/config/ConfigGui.java
@@ -0,0 +1,35 @@
+/**
+ * This file is a part of FrogCraftRebirth, 
+ * created by 3TUSK at 7:38:36 PM, Jul 25, 2016, 
+ * FrogCraftRebirth, is open-source under MIT license,
+ * check https://github.com/FrogCraft-Rebirth/
+ * FrogCraft-Rebirth/LICENSE_FrogCraft_Rebirth for 
+ * more information.
+ */
+package frogcraftrebirth.common.lib.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import frogcraftrebirth.api.FrogAPI;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.fml.client.config.GuiConfig;
+import net.minecraftforge.fml.client.config.IConfigElement;
+
+public class ConfigGui extends GuiConfig {
+	
+	private static final List<IConfigElement> ELEMENTS_LIST;
+	
+	static {
+		ELEMENTS_LIST = new ArrayList<IConfigElement>();
+		ELEMENTS_LIST.add(new ConfigElement(ConfigMain.config.getCategory("Machine")));
+		ELEMENTS_LIST.add(new ConfigElement(ConfigMain.config.getCategory("Misc")));
+		ELEMENTS_LIST.add(new ConfigElement(ConfigMain.config.getCategory("Compatibility")));
+	}
+
+	public ConfigGui(GuiScreen parentScreen) {
+		super(parentScreen, ELEMENTS_LIST, FrogAPI.MODID, false, false, "FrogCraft-Rebirth", "Config Interface");
+	}
+
+}
---
+++ src/main/java/frogcraftrebirth/common/lib/config/ConfigGuiFactory.java
@@ -0,0 +1,39 @@
+/**
+ * This file is a part of FrogCraftRebirth, 
+ * created by 3TUSK at 7:47:01 PM, Jul 25, 2016, 
+ * FrogCraftRebirth, is open-source under MIT license,
+ * check https://github.com/FrogCraft-Rebirth/
+ * FrogCraft-Rebirth/LICENSE_FrogCraft_Rebirth for 
+ * more information.
+ */
+package frogcraftrebirth.common.lib.config;
+
+import java.util.Set;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.IModGuiFactory;
+
+public class ConfigGuiFactory implements IModGuiFactory {
+
+	@Override
+	public void initialize(Minecraft minecraftInstance) {
+		
+	}
+
+	@Override
+	public Class<? extends GuiScreen> mainConfigGuiClass() {
+		return ConfigGui.class;
+	}
+
+	@Override
+	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
+		return null;
+	}
+
+	@Override
+	public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
+		return null;
+	}
+
+}

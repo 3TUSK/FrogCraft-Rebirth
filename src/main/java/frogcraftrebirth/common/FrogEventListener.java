059e672107f8ab885bac66757743ce5d862b4f00
3TUSK <urey.s.knowledge@gmail.com>
2016-07-27 18:49:50 +0800

    OK we have a barely functional achievement system now

--- src/main/java/frogcraftrebirth/common/FrogEventListener.java
+++ src/main/java/frogcraftrebirth/common/FrogEventListener.java
@@ -1,13 +1,15 @@
 package frogcraftrebirth.common;
 
 import frogcraftrebirth.api.FrogAchievements;
-import frogcraftrebirth.api.event.PersonalizationEvent;
+import frogcraftrebirth.api.event.AccessControlEvent;
 import frogcraftrebirth.common.item.ItemIngot;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -34,13 +36,52 @@ public class FrogEventListener {
 	
 	@SubscribeEvent
 	public void onCrafting(ItemCraftedEvent event) {
-		if (event.crafting.getItem() instanceof ItemBlock) {
-			//TODO
+		ItemStack crafted = event.crafting;
+		if (crafted.getItem() instanceof ItemBlock) {
+			Block block = ((ItemBlock)crafted.getItem()).block;
+			
+			if (block == FrogBlocks.machines)
+				switch (block.getMetaFromState(block.getBlockState().getBaseState()) & 0b11) {
+					case 0: {
+						event.player.addStat(FrogAchievements.ADV_CHEM_REACTOR.get());
+						return;
+					}
+					case 1: {
+						event.player.addStat(FrogAchievements.GAS_PUMP.get());
+						return;
+					}
+					case 3: {
+						event.player.addStat(FrogAchievements.LIQUEFIER.get());
+						return;
+					}
+				}
+			
+			if (block == FrogBlocks.condenseTowerPart)
+				if ((block.getMetaFromState(block.getBlockState().getBaseState()) & 0b11) == 0) {
+					event.player.addStat(FrogAchievements.CONDENSE_TOWER_CORE.get());
+					return;
+				}
+			if (block == FrogBlocks.hybridStorageUnit)
+				switch (block.getMetaFromState(block.getBlockState().getBaseState()) % 2) {
+					case 0: {
+						event.player.addStat(FrogAchievements.HSU.get());
+						return;
+					}
+					case 1: {
+						event.player.addStat(FrogAchievements.UHSU.get());
+						return;
+					}
+				}
+		} else {
+			if (crafted.getItem() == FrogItems.jinkela) {
+				event.player.addStat(FrogAchievements.JINKELA.get());
+				return;
+			}
 		}
 	}
 	
 	@SubscribeEvent
-	public void onPersonalize(PersonalizationEvent event) {
+	public void onPersonalize(AccessControlEvent event) {
 		if (event.tile.getOwnerUUID() == null)
 			event.tile.setOwner(event.player.getUniqueID());
 	}
--- src/main/java/frogcraftrebirth/common/registry/RegFrogAchievements.java
+++ src/main/java/frogcraftrebirth/common/registry/RegFrogAchievements.java
@@ -12,7 +12,7 @@ public class RegFrogAchievements {
 	
 	public static void init() {
 		AchievementPage.registerAchievementPage(new AchievementPage(FrogAPI.MODID, 
-				EVT, RAILGUN, POTASSIUM, GAS_PUMP, /*PNEUMATIC_COMPRESSOR,*/ LIQUIFIER, HSU, UHSU, ADV_CHEM_REACTOR, JINKELA, CONDENSE_TOWER_CORE, CONDENSE_TOWER, NITRIC_ACID));
+				EVT, RAILGUN, POTASSIUM, GAS_PUMP, /*PNEUMATIC_COMPRESSOR,*/ LIQUEFIER, HSU, UHSU, ADV_CHEM_REACTOR, JINKELA, CONDENSE_TOWER_CORE, CONDENSE_TOWER, NITRIC_ACID));
 	}
 	
 	static final Achievement EVT = new Achievement("EVT", "get_EVT", 0, 0, new ItemStack(FrogBlocks.hybridStorageUnit), null);
@@ -20,12 +20,12 @@ public class RegFrogAchievements {
 	static final Achievement POTASSIUM = new Achievement("POTASSIUM", "killed_by_Potassium", -2, 0, new ItemStack(FrogItems.itemIngot, 1, 0), null);
 	static final Achievement GAS_PUMP = new Achievement("GAS_PUMP", "gaspump", 2, 0, new ItemStack(FrogBlocks.machines, 1, 1), null);
 	//static final Achievement PNEUMATIC_COMPRESSOR = new Achievement("PNEUMATIC_COMPRESSOR", "pneumaticCompressor", 2, 2, FrogItems.pneumaticCompressor, GAS_PUMP);
-	static final Achievement LIQUIFIER = new Achievement("LIQUIFER", "liquifier", 2, -2, new ItemStack(FrogBlocks.machines), GAS_PUMP);
+	static final Achievement LIQUEFIER = new Achievement("LIQUIFER", "liquifier", 2, -2, new ItemStack(FrogBlocks.machines), GAS_PUMP);
 	static final Achievement HSU = new Achievement("HSU", "hsu", 0, 2, new ItemStack(FrogBlocks.hybridStorageUnit), EVT);
 	static final Achievement UHSU = new Achievement("UHSU", "uhsu", 0, 4, new ItemStack(FrogBlocks.hybridStorageUnit, 1, 1), HSU);
 	static final Achievement ADV_CHEM_REACTOR = new Achievement("ADV_CHEM_REACTOR", "get_ACR", 4, 0, new ItemStack(FrogBlocks.machines, 1, 0), GAS_PUMP);
 	static final Achievement JINKELA = new Achievement("JINKELA", "goldclod", 4, -2, new ItemStack(FrogItems.jinkela), ADV_CHEM_REACTOR);
-	static final Achievement CONDENSE_TOWER_CORE = new Achievement("CONDENSE_TOWER_CORE", "condensetower", 2, -4, new ItemStack(FrogBlocks.condenseTowerPart), LIQUIFIER);
+	static final Achievement CONDENSE_TOWER_CORE = new Achievement("CONDENSE_TOWER_CORE", "condensetower", 2, -4, new ItemStack(FrogBlocks.condenseTowerPart), LIQUEFIER);
 	static final Achievement CONDENSE_TOWER = new Achievement("CONDENSE_TOWER", "condensetower2", 4, -4, new ItemStack(FrogBlocks.condenseTowerPart), CONDENSE_TOWER_CORE);
 	static final Achievement NITRIC_ACID = new Achievement("NITRIC_ACID", "suicideExpert", -2, 2, new ItemStack(FrogBlocks.fluidNitricAcid), POTASSIUM);
 

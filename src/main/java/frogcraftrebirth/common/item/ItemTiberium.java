887eae1b25f6fc2519d4e97c180eaf947438a47a
3TUSK <urey.s.knowledge@gmail.com>
2016-08-04 14:21:03 +0800

    Close #19
    Also upload some temporary obj model. Otherwise, you cannot tell where
    is tiberium crystal by just looking around.

--- src/main/java/frogcraftrebirth/common/item/ItemTiberium.java
+++ src/main/java/frogcraftrebirth/common/item/ItemTiberium.java
@@ -13,8 +13,13 @@ import java.util.List;
 
 import frogcraftrebirth.api.FrogAPI;
 import frogcraftrebirth.common.lib.item.ItemFrogCraft;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 
 public class ItemTiberium extends ItemFrogCraft {
 
@@ -28,12 +33,29 @@ public class ItemTiberium extends ItemFrogCraft {
 
 	@Override
 	public List<String> getToolTip(ItemStack stack, EntityPlayer player, boolean adv) {
-		return Arrays.asList("CAUTION: NOT FULLY IMPLEMENTED YET");
+		return Arrays.asList(I18n.format("item.tiberium.info"));
 	}
 	
 	@Override
 	public String getUnlocalizedName(ItemStack stack) {
 		return super.getUnlocalizedName()+"."+nameArray[stack.getMetadata()];
+	}
+	
+	@Override
+	public void onUpdate(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected) {
+		if (world.rand.nextInt(100) == 0 && entity instanceof EntityPlayerMP) {
+			ItemStack[] inv = ((EntityPlayerMP)entity).inventory.mainInventory;
+			for (int index = 0; index < 36; index++) {
+				if (world.rand.nextInt(100) == 0) {
+					ItemStack target = inv[index];
+					if (target != null && !(target.getItem() instanceof ItemTiberium)) {
+						inv[index] = new ItemStack(this, target.stackSize, stack.getMetadata());
+						entity.addChatMessage(new TextComponentTranslation("chat.tiberiumCorrosion"));
+					}
+				}
+			}
+			
+		}
 	}
 
 }

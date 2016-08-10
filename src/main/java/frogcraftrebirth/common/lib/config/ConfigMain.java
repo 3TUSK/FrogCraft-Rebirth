1c68b053194398e78e8008b37d8b6e108441beed
3TUSK <urey.s.knowledge@gmail.com>
2016-08-11 05:26:22 +0800

    Mark ConfigMain as awaitng for overhaul

--- src/main/java/frogcraftrebirth/common/lib/config/ConfigMain.java
+++ src/main/java/frogcraftrebirth/common/lib/config/ConfigMain.java
@@ -6,7 +6,13 @@ import frogcraftrebirth.api.FrogAPI;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class ConfigMain {
+public final class ConfigMain {
+	
+	private ConfigMain() {
+		throw new UnsupportedOperationException();
+	}
+	
+	//Require a thorough clean up	
 	
 	static Configuration config;
 
@@ -24,24 +30,19 @@ public class ConfigMain {
 	public static boolean enableClassicMode;
 	
 	public static void init(FMLPreInitializationEvent event) {
-		File configDir = new File(event.getModConfigurationDirectory(), FrogAPI.MODID);
-		
+		File configDir = new File(event.getModConfigurationDirectory(), FrogAPI.MODID);	
 		if (!configDir.exists())
 			configDir.mkdirs();
 		ConfigMain.initMainConfig(new File(configDir, "FrogMain.cfg"));
-		
-		File reactionDir = new File(configDir, "reactions");
-		if (!reactionDir.exists())
-			reactionDir.mkdirs(); // This is left for legacy reasons.
 	}
 	
-	public static void initMainConfig(File file) {
+	private static void initMainConfig(File file) {
 		config = new Configuration(file);
 		
 		config.load();
 		
 		enableModpackCreationMode = config.getBoolean("EnableModpackCreationMode", "General", false, "Set this to ture will let FrogCraft: Rebirth not loading any recipes, providing convenience for modpack creators.", "frogcraft.config.general.modpack");
-		enableClassicMode = config.get("General", "EnableClassicMode", false).getBoolean();
+		enableClassicMode = config.get("General", "EnableClassicMode", false, "").getBoolean();
 		
 		airPumpPowerRate = config.get("Machine", "AirPumpPowerRate", 120).getInt();
 		airPumpGenerateSpeed = config.get("Machine", "AirPumpGenerateSpeed", 50).getInt();

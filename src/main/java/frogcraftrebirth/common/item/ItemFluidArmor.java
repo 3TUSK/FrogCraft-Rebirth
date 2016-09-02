32ea2b5bbb9b89b31bbe10454f4c3c50b1619c1d
3TUSK <urey.s.knowledge@gmail.com>
2016-09-02 17:09:08 -0400

    Add ISpecialArmor support

--- src/main/java/frogcraftrebirth/common/item/ItemFluidArmor.java
+++ src/main/java/frogcraftrebirth/common/item/ItemFluidArmor.java
@@ -7,13 +7,16 @@ import frogcraftrebirth.api.FrogAPI;
 import frogcraftrebirth.api.item.FluidArmorPotionEffectManager;
 import ic2.api.item.IMetalArmor;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ISpecialArmor;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fluids.FluidStack;
@@ -24,14 +27,14 @@ import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStack;
  * @author 3TUSK
  * Created at 7:46:59 PM Sep 6, 2015 EST 2015
  */
-public class ItemFluidArmor extends ItemArmor implements IMetalArmor {
+public class ItemFluidArmor extends ItemArmor implements IMetalArmor, ISpecialArmor {
 
-	public static final ArmorMaterial FLUID_ARMOR = EnumHelper.addArmorMaterial("fluidArmor", "armorMaterial.fluidArmor", 1000, new int[] {1,1,1,1}, 5, null, 1);
+	public static final ArmorMaterial FLUID_ARMOR = EnumHelper.addArmorMaterial("fluidArmor", "armorMaterial.fluidArmor", 10, new int[] {0,0,0,0}, 5, null, 1);
 	
 	public final int capacity;
 	
 	public ItemFluidArmor(int capacity) {
-		super(FLUID_ARMOR, 0, EntityEquipmentSlot.CHEST);//0->cloth(leather) renderer; render stuff is WIP
+		super(FLUID_ARMOR, 0, EntityEquipmentSlot.CHEST);
 		setMaxStackSize(1);
 		setCreativeTab(FrogAPI.TAB);
 		setRegistryName("fluid_armor");
@@ -43,7 +46,6 @@ public class ItemFluidArmor extends ItemArmor implements IMetalArmor {
 	public String getArmorTexture(ItemStack stack, Entity entity, EntityEquipmentSlot slot, String type) {
 		return "frogcraftrebirth:textures/items/FluidArmor.png";
 	}
-	
 
 	@Override
 	public boolean isMetalArmor(ItemStack itemstack, EntityPlayer player) {
@@ -78,7 +80,20 @@ public class ItemFluidArmor extends ItemArmor implements IMetalArmor {
 			info.add("No fluid is in armor now");
 		}
 	}
-	
-	
-	
+
+	@Override
+	public ArmorProperties getProperties(EntityLivingBase player, ItemStack armor, DamageSource source, double damage, int slot) {
+		return new ArmorProperties(0, 1.0, 100);
+	}
+
+	@Override
+	public int getArmorDisplay(EntityPlayer player, ItemStack armor, int slot) {
+		return 0;
+	}
+
+	@Override
+	public void damageArmor(EntityLivingBase entity, ItemStack stack, DamageSource source, int damage, int slot) {
+		
+	}
+
 }

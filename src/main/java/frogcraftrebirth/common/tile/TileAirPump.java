87b056051bc8824dc32001fbba46385cb55ce06e
3TUSK <urey.s.knowledge@gmail.com>
2016-12-16 10:41:40 -0500

    Let TileAirPump extends TileEnergySink

--- src/main/java/frogcraftrebirth/common/tile/TileAirPump.java
+++ src/main/java/frogcraftrebirth/common/tile/TileAirPump.java
@@ -7,49 +7,35 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-import frogcraftrebirth.common.lib.tile.TileFrog;
-import ic2.api.energy.event.EnergyTileLoadEvent;
-import ic2.api.energy.event.EnergyTileUnloadEvent;
+import frogcraftrebirth.common.lib.tile.TileEnergySink;
 import ic2.api.energy.tile.IEnergyEmitter;
-import ic2.api.energy.tile.IEnergySink;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class TileAirPump extends TileFrog implements ITickable, IEnergySink, IAirPump, IHasWork {
-	
+public class TileAirPump extends TileEnergySink implements ITickable, IAirPump, IHasWork {
+
 	private static final int MAX_AIR = 1000;
 	private static final int MAX_CHARGE = 10000;
 	
 	public int charge;
 	private int airAmount, tick;
-	private boolean isInENet;
+	
+	public TileAirPump() {
+		super(1, MAX_CHARGE);
+	}
 	
 	@Override
 	public boolean isWorking() {
 		return getWorld().isBlockIndirectlyGettingPowered(this.pos) == 0;
 	}
 	
-	public void invalidate() {
-		if (!getWorld().isRemote && isInENet) {
-			MinecraftForge.EVENT_BUS.post(new EnergyTileUnloadEvent(this));
-			isInENet = false;
-		}
-		super.invalidate();
-	}
-	
 	@Override
 	public void update() {
 		if (getWorld().isRemote) {
 			return;
-		}
-		
-		if (!isInENet) {
-			MinecraftForge.EVENT_BUS.post(new EnergyTileLoadEvent(this));
-			isInENet = true;
 		}
 		
 		if (this.getWorld().isBlockIndirectlyGettingPowered(this.pos) != 0)
@@ -75,13 +61,11 @@ public class TileAirPump extends TileFrog implements ITickable, IEnergySink, IAi
 	
 	public void readFromNBT(NBTTagCompound tag) {
 		super.readFromNBT(tag);
-		this.charge = tag.getInteger("charge");
 		this.airAmount = tag.getInteger("air");
 		this.tick = tag.getInteger("tick");
 	}
 	
 	public NBTTagCompound writeToNBT(NBTTagCompound tag) {
-		tag.setInteger("charge", this.charge);
 		tag.setInteger("air", this.airAmount);
 		tag.setInteger("tick", this.tick);
 		return super.writeToNBT(tag);
@@ -89,7 +73,7 @@ public class TileAirPump extends TileFrog implements ITickable, IEnergySink, IAi
 	
 	@Override
 	public void writePacketData(DataOutputStream output) throws IOException {
-		output.writeInt(charge);
+		super.writePacketData(output);
 		output.writeInt(airAmount);
 		output.writeInt(tick);
 	}
@@ -97,7 +81,7 @@ public class TileAirPump extends TileFrog implements ITickable, IEnergySink, IAi
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void readPacketData(DataInputStream input) throws IOException {
-		this.charge = input.readInt();
+		super.readPacketData(input);
 		this.airAmount = input.readInt();
 		this.tick = input.readInt();
 	}
@@ -105,24 +89,6 @@ public class TileAirPump extends TileFrog implements ITickable, IEnergySink, IAi
 	@Override
 	public boolean acceptsEnergyFrom(IEnergyEmitter emitter, EnumFacing direction) {
 		return direction != EnumFacing.UP;
-	}
-
-	@Override
-	public double getDemandedEnergy() {
-		return MAX_CHARGE - this.charge;
-	}
-
-	@Override
-	public int getSinkTier() {
-		return 1;
-	}
-
-	@Override
-	public double injectEnergy(EnumFacing directionFrom, double amount, double voltage) {
-		this.charge += amount;
-		if (this.charge >= MAX_CHARGE)
-			this.charge = MAX_CHARGE;
-		return 0;	
 	}
 
 	@Override

88997cb8e2b955e1c5453a11e360fb13aec655dd
3TUSK <urey.s.knowledge@gmail.com>
2017-09-05 21:05:52 +0800

    Behold, those recipes are striking back

--- src/main/java/frogcraftrebirth/common/registry/RegFluid.java
+++ src/main/java/frogcraftrebirth/common/registry/RegFluid.java
@@ -25,7 +25,9 @@ public class RegFluid {
 		FrogFluids.carbonDioxide = new FrogFluid("carbon_dioxide", 1980, 300, true, EnumRarity.COMMON);
 		FrogFluids.coalTar = new FrogFluid("coal_tar", 2000, 300, false, EnumRarity.RARE).setViscosity(2000);
 		FrogFluids.fluorine = new FrogFluid("fluorine", 1696, 300, true, EnumRarity.EPIC);
+		FrogFluids.methane = new FrogFluid("methane", 656, 300, true, EnumRarity.UNCOMMON);
 		FrogFluids.nitricAcid = new FrogFluid("nitric_acid", "nitric_acid_flow", "nitric_acid",  1420, 300, false, EnumRarity.RARE);
+		FrogFluids.nitrogen = new FrogFluid("nitrogen", 1251, 160, true, EnumRarity.COMMON);
 		FrogFluids.nitrogenOxide = new FrogFluid("nitrogen_oxide", 1340, 300, true, EnumRarity.RARE);
 		FrogFluids.oxygen = new FrogFluid("oxygen", 1429, 300, true, EnumRarity.COMMON);
 		FrogFluids.sulfurDioxide = new FrogFluid("sulfur_dioxide", 1640, 300, true, EnumRarity.UNCOMMON);
@@ -39,7 +41,9 @@ public class RegFluid {
 		regFluid(FrogFluids.carbonDioxide);
 		regFluid(FrogFluids.coalTar, true);
 		regFluid(FrogFluids.fluorine);
+		regFluid(FrogFluids.methane);
 		regFluid(FrogFluids.nitricAcid, true, fluid -> new BlockNitricAcid(fluid).setRegistryName("nitric_acid"));
+		regFluid(FrogFluids.nitrogen);
 		regFluid(FrogFluids.nitrogenOxide);
 		regFluid(FrogFluids.oxygen);
 		regFluid(FrogFluids.sulfurDioxide);
--- src/main/java/frogcraftrebirth/common/registry/RegFrogRecipes.java
+++ src/main/java/frogcraftrebirth/common/registry/RegFrogRecipes.java
@@ -11,6 +11,7 @@ import frogcraftrebirth.common.lib.PyrolyzerRecipe;
 import frogcraftrebirth.common.lib.recipes.FrogRecipeInputFluidStack;
 import frogcraftrebirth.common.lib.recipes.FrogRecipeInputItemStack;
 import frogcraftrebirth.common.lib.recipes.FrogRecipeInputUniversalFluidCell;
+import frogcraftrebirth.common.lib.recipes.FrogRecipeInputs;
 import ic2.api.item.IC2Items;
 import ic2.api.recipe.IRecipeInputFactory;
 import ic2.api.recipe.Recipes;
@@ -20,6 +21,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.oredict.OreDictionary;
 
 import java.util.Arrays;
@@ -30,11 +32,40 @@ public class RegFrogRecipes {
 	public static void init() {
 		initOreDict();
 		if (!FrogConfig.modpackMode) {
+			// CaO + H2O -> Ca(OH)2
 			FrogAPI.managerACR.add(new AdvChemRecRecipe(Arrays.asList(new FrogRecipeInputItemStack(new ItemStack(FrogRegistees.DUST, 1,2)), new FrogRecipeInputUniversalFluidCell(new FluidStack(FluidRegistry.WATER, 1000))), Collections.singleton(new ItemStack(FrogRegistees.DUST, 1,3)), ItemStack.EMPTY, 20, 100, 0, 1));
-			FrogAPI.managerCT.add(new CondenseTowerRecipe(100, 75, new FluidStack(FrogFluids.coalTar, 5), new FluidStack[] { new FluidStack(FrogFluids.benzene, 2), new FluidStack(FrogFluids.ammonia, 1), new FluidStack(FrogFluids.carbonOxide, 2) }));
-			FrogAPI.managerCT.add(new CondenseTowerRecipe(10, 75, new FluidStack(FluidRegistry.getFluid("ic2air"), 10), new FluidStack[] { new FluidStack(FrogFluids.argon, 1), new FluidStack(FrogFluids.oxygen, 7), new FluidStack(FrogFluids.carbonDioxide, 2) }));
+			// Ca(OH)2 + CO2 -> CaCO3 + H2O
+			FrogAPI.managerACR.add(new AdvChemRecRecipe(Arrays.asList(new FrogRecipeInputItemStack(new ItemStack(FrogRegistees.DUST, 1, 3)), new FrogRecipeInputUniversalFluidCell(new FluidStack(FrogFluids.carbonDioxide, 1000))), Arrays.asList(ItemStack.EMPTY, IC2Items.getItem("fluid_cell", "water")), ItemStack.EMPTY, 20, 10, 0, 0));
+			// N2 + 3H2 -> 2NH3
+			ItemStack hydrogenCells = IC2Items.getItem("fluid_cell", "ic2hydrogen");
+			hydrogenCells.setCount(3);
+			ItemStack ammoniaCell = FrogRecipeInputs.UNI_CELL.copy();
+			FluidUtil.getFluidHandler(ammoniaCell).fill(new FluidStack(FrogFluids.ammonia, 1000), true);
+			ammoniaCell.setCount(2);
+			FrogAPI.managerACR.add(new AdvChemRecRecipe(Arrays.asList(new FrogRecipeInputUniversalFluidCell(new FluidStack(FrogFluids.nitrogen, 1000)), new FrogRecipeInputItemStack(hydrogenCells)), Collections.singleton(ammoniaCell), new ItemStack(FrogRegistees.REACTION_MODULE, 1, 2), 150, 128, 0, 2));
+			// CO2 + 2NH3 -> CO(NH2)2 + H2O
+			FrogAPI.managerACR.add(new AdvChemRecRecipe(Arrays.asList(new FrogRecipeInputUniversalFluidCell(new FluidStack(FrogFluids.carbonDioxide, 1000)), new FrogRecipeInputItemStack(ammoniaCell.copy())), Arrays.asList(new ItemStack(FrogRegistees.DUST, 1, 13), IC2Items.getItem("fluid_cell", "water")), ItemStack.EMPTY,50, 40, 0, 2));
+			// HNO3 + NH3 -> NH4NO3
+			FrogAPI.managerACR.add(new AdvChemRecRecipe(Arrays.asList(new FrogRecipeInputUniversalFluidCell(new FluidStack(FrogFluids.nitricAcid, 1000)), new FrogRecipeInputUniversalFluidCell(new FluidStack(FrogFluids.ammonia, 1000))), Collections.singleton(new ItemStack(FrogRegistees.DUST, 1, 11)), ItemStack.EMPTY, 10, 10, 0, 2));
+			//C+O2=CO2
+			//Ca3(PO4)2 + 3SiO2 + 5 C == 3 CaSiO3 + 5CO + 2P
+			//2CO+O2=2CO2
+			//CaSiO3=CaO+SiO2
+			//C->raw carbon fiber
+			//Mg+Br2=MgBr2
+			//Cl2+2K=2KCl
+			//CaF2=Ca+F2
+			//Ca+F2=CaF2
+			//SO3+H2O->H2SO4  TODO: utilize oleum - Vexatos, I am looking at you!
+			//2SO2+O2->2SO3
+
+			FrogAPI.managerCT.add(new CondenseTowerRecipe(100, 75, new FluidStack(FrogFluids.coalTar, 25), new FluidStack[] { new FluidStack(FrogFluids.benzene, 2), new FluidStack(FrogFluids.ammonia, 3), new FluidStack(FrogFluids.carbonOxide, 5), new FluidStack(FrogFluids.methane, 10), FluidRegistry.getFluidStack("ic2hydrogen", 5) }));
+			FrogAPI.managerCT.add(new CondenseTowerRecipe(10, 75, new FluidStack(FluidRegistry.getFluid("ic2air"), 12), new FluidStack[] { new FluidStack(FrogFluids.argon, 1), new FluidStack(FrogFluids.nitrogen, 7), new FluidStack(FrogFluids.oxygen, 2), new FluidStack(FrogFluids.carbonDioxide, 2) }));
+
 			FrogAPI.managerPyrolyzer.add(new PyrolyzerRecipe(IC2Items.getItem("dust", "coal"), new ItemStack(FrogRegistees.INGOT, 1, 4), new FluidStack(FrogFluids.coalTar, 50), 80, 48));
 			FrogAPI.managerPyrolyzer.add(new PyrolyzerRecipe(new ItemStack(Blocks.COBBLESTONE), new ItemStack(FrogRegistees.DUST, 1, 2), new FluidStack(FrogFluids.carbonDioxide, 50), 100, 64));
+			FrogAPI.managerPyrolyzer.add(new PyrolyzerRecipe(new ItemStack(Blocks.STONE), new ItemStack(FrogRegistees.DUST, 1, 2), new FluidStack(FrogFluids.carbonDioxide, 50), 100, 64));
+
 			FrogAPI.FUEL_REG.regFuelByproduct(new ItemStack(Items.COAL, 1, 0), FrogFluids.carbonDioxide);
 			FrogAPI.FUEL_REG.regFuelByproduct(new ItemStack(Items.COAL, 1, 1), FrogFluids.carbonDioxide);
 			FrogAPI.FUEL_REG.regFuelByproduct("dustCoal", FrogFluids.carbonDioxide);
@@ -49,7 +80,8 @@ public class RegFrogRecipes {
 		MPSUpgradeManager.INSTANCE.registerVoltageUpgrades(IC2Items.getItem("upgrade", "transformer"), 1);
 
 		Recipes.advRecipes.addRecipe(new ItemStack(FrogRegistees.AMMONIA_COOLANT_60K), " T ", "TCT", " T ", 'T', "plateTin", 'C', new FluidStack(FrogFluids.ammonia, 1000));
-		
+		Recipes.advRecipes.addRecipe(new ItemStack(FrogRegistees.JINKELA), "KKK", "PPP", "NNN", 'K', "ingotPotassium", 'P', "ingotPhosphorus", 'N', new FluidStack(FrogFluids.nitrogen, 1000));
+
 		Recipes.macerator.addRecipe(Recipes.inputFactory.forStack(new ItemStack(FrogRegistees.ORE, 1, 0)), null, true, new ItemStack(FrogRegistees.CRUSHED_DUST, 3, 0));
 		Recipes.macerator.addRecipe(Recipes.inputFactory.forStack(new ItemStack(FrogRegistees.ORE, 1, 1)), null, true, new ItemStack(FrogRegistees.CRUSHED_DUST, 3, 1));
 		Recipes.macerator.addRecipe(Recipes.inputFactory.forStack(new ItemStack(FrogRegistees.ORE, 1, 2)), null, true, new ItemStack(FrogRegistees.CRUSHED_DUST, 3, 2));
@@ -75,7 +107,10 @@ public class RegFrogRecipes {
 	
 	static void initOreDict() {
 		OreDictionary.registerOre("jinkela", FrogRegistees.JINKELA);
-		
+
+		OreDictionary.registerOre("ingotPotassium", new ItemStack(FrogRegistees.INGOT, 1, 0));
+		OreDictionary.registerOre("ingotPhosphorus", new ItemStack(FrogRegistees.INGOT, 1, 1));
+
 		OreDictionary.registerOre("crystalTiberiumRed", new ItemStack(FrogRegistees.TIBERIUM, 1, 0));
 		OreDictionary.registerOre("crystalTiberiumBlue", new ItemStack(FrogRegistees.TIBERIUM, 1, 1));
 		OreDictionary.registerOre("crystalTiberiumGreen", new ItemStack(FrogRegistees.TIBERIUM, 1, 2));

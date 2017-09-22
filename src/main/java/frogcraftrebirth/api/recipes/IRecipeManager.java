9f7277f209ffbf4c4f6d8a31e6b435d9b5fa8ed3
3TUSK <urey.s.knowledge@gmail.com>
2017-09-22 08:09:23 -0700

    Binary compatibility

--- src/main/java/frogcraftrebirth/api/recipes/IRecipeManager.java
+++ src/main/java/frogcraftrebirth/api/recipes/IRecipeManager.java
@@ -1,10 +1,12 @@
 package frogcraftrebirth.api.recipes;
 
-import java.util.Arrays;
+import javax.annotation.Nullable;
 import java.util.Collection;
 
 /**
- *
+ * A <code>RecipeManager</code> represents a manager
+ * dedicated for a certain type of recipe (which is
+ * represented by type parameter, R).
  * @since 1.0.0
  */
 public interface IRecipeManager<R> {
@@ -16,17 +18,36 @@ public interface IRecipeManager<R> {
 	Collection<R> getRecipes();
 
 	/**
+	 * @implSpec
+	 * Default implementation always returns null, for binary-compatible sake.
+	 *
 	 * @param input Array of wrapped inputs
 	 *
-	 * @implSpec
-	 * Default implementation delegates to {@link #getRecipe(Iterable)}.
-	 *
 	 * @return The recipe if matched any; null if otherwise.
+	 *
+	 * @see #getRecipe(Iterable) 
 	 */
+	@Nullable
 	default R getRecipe(IFrogRecipeInput... input) {
-		return getRecipe(Arrays.asList(input));
+		return null;
 	}
 
-	R getRecipe(Iterable<IFrogRecipeInput> input);
+	/**
+	 * {@link Iterable} variant of {@link #getRecipe(IFrogRecipeInput...)},
+	 * useful when the inputs are in a {@link java.util.Collection}.
+	 *
+	 * @implSpec
+	 * Default implementation always returns null, for binary-compatible sake.
+	 *
+	 * @param input Array of wrapped inputs
+	 *
+	 * @return The recipe if matched any; null if otherwise.
+	 *
+	 * @see #getRecipe(IFrogRecipeInput...)
+	 */
+	@Nullable
+	default R getRecipe(Iterable<IFrogRecipeInput> input) {
+		return null;
+	}
 
 }
